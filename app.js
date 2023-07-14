a64cdb260ba79f3a9b1d44f7d2b59b4005c86ad4
Aditya30december2003 <94634290+Aditya30december2003@users.noreply.github.com>
2023-07-14 17:53:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,77 +1,77 @@
 let comics=[
     {
         id:1,
-        img:"../Filter3/imgs/spiderman1.jpg",
+        img:"imgs/spiderman1.jpg",
         category:"Spiderman"
     },
     {
         id:2,
-        img:"../Filter3/imgs/f4-2.jpg",
+        img:"imgs/f4-2.jpg",
         category:"Fantastic Four"
     },
     {
         id:3,
-        img:"../Filter3/imgs/ironman1.jpg",
+        img:"imgs/ironman1.jpg",
         category:"Ironman"
     },
     {
         id:4,
-        img:"../Filter3/imgs/xmen2.jpg",
+        img:"imgs/xmen2.jpg",
         category:"X-men"
     },
     {
         id:5,
-        img:"../Filter3/imgs/hulk1.jpg",
+        img:"imgs/hulk1.jpg",
         category:"Hulk"
     },
     {
         id:6,
-        img:"../Filter3/imgs/xmen3.jpg",
+        img:"imgs/xmen3.jpg",
         category:"X-men"
     },
     {
         id:7,
-        img:"../Filter3/imgs/f4-1.jpg",
+        img:"imgs/f4-1.jpg",
         category:"Fantastic Four"
     },
     {
         id:8,
-        img:"../Filter3/imgs/avengers2.jpg",
+        img:"imgs/avengers2.jpg",
         category:"Avengers"
     },
     {
         id:9,
-        img:"../Filter3/imgs/spiderman3.jpg",
+        img:"imgs/spiderman3.jpg",
         category:"Spiderman"
     },
     {
         id:10,
-        img:"../Filter3/imgs/hulk4.jpg",
+        img:"imgs/hulk4.jpg",
         category:"Hulk"
     },
     {
         id:11,
-        img:"../Filter3/imgs/xmen.jpg",
+        img:"imgs/xmen.jpg",
         category:"X-men"
     },
     {
         id:12,
-        img:"../Filter3/imgs/f4-3.jpg",
+        img:"imgs/f4-3.jpg",
         category:"Fantastic Four"
     },
     {
         id:13,
-        img:"../Filter3/imgs/ironman3.jpg",
+        img:"imgs/ironman3.jpg",
         category:"Ironman"
     },
     {
         id:14,
-        img:"../Filter3/imgs/avengers3.jpg",
+        img:"imgs/avengers3.jpg",
         category:"Avengers"
     },
     {
         id:15,
-        img:"../Filter3/imgs/ironman2.jpg",
+        img:"imgs/ironman2.jpg",
         category:"Ironman"
     }
 ]
